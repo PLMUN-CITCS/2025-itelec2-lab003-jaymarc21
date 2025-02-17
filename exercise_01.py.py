96a216a7ec3d2c6aef0deeced4a0f07805735ce4
jaymarc21 <custodiojaymarc@_bsitplmun.edu.ph>
2025-02-17 18:48:26 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

---
+++ exercise_01.py.py
@@ -0,0 +1,14 @@
+# Custodio, Jay Marc
+# ITELEC2
+
+message = "Hello, Python!" 
+is_active = True  
+result = None 
+total_count = 20    
+print("Integer (count):", count)
+print("Integer (Count):", Count)
+print("Integer (total_count):", total_count)
+print("Decimal:", decimal_value)
+print("Text:", message)
+print("Boolean:", is_active)
+print("None Value:", result)
