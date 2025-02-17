090e00b0a33a434a9e6ff3cf8b155b3195701d2e
jaymarc21 <custodiojaymarc@_bsitplmun.edu.ph>
2025-02-17 20:14:30 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

--- exercise_01.py
+++ exercise_01.py
@@ -1,12 +1,12 @@
 # Custodio, Jay Marc
 # ITELEC2
 
-message = "Hello, Python!" 
-is_active = True  
-result = None 
-total_count = 20    
-print("Integer (count):", count)
-print("Integer (Count):", Count)
+message = "Hello, Python!"
+is_active = True
+result = None
+total_count = 20
+decimal_value = 10.5  # Define a decimal value
+
 print("Integer (total_count):", total_count)
 print("Decimal:", decimal_value)
 print("Text:", message)
